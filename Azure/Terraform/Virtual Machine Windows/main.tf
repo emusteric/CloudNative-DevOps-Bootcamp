479efab5cf6c88472589e321368be0dc1a582e28
emusteric <ben.hollamby@outlook.com>
2021-03-06 11:17:52 +1300

    main.tf is now all variables except for VM name which was failing

--- Azure/Terraform/Virtual Machine Windows/main.tf
+++ Azure/Terraform/Virtual Machine Windows/main.tf
@@ -4,17 +4,17 @@ provider "azurerm" {
 
 #Resource Group
 resource "azurerm_resource_group" "rg" {
-  name = "benchallengerg"
-  location = "australiaeast"
+  name = var.resource_group_name
+  location = var.location
   tags = {
-    "Cloudskills" = "Challenge"
+    "CloudSkills" = "Challenge"
   }
 }
 
 #Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-    name = "challenge-network"
-    address_space = [ "10.0.0.0/16" ]
+    name = var.vnet_name
+    address_space = var.address_space
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     tags = {
@@ -24,50 +24,48 @@ resource "azurerm_virtual_network" "vnet" {
 
 #Subnet
 resource "azurerm_subnet" "subnet" {
-    name = "internal_subnet"
+    name = var.subnet_name
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = [ "10.0.1.0/24" ]
+    address_prefixes = var.subnet_address_range
 }
 
 #Network Interface
 resource "azurerm_network_interface" "nic" {
-    name = "vmnic"
+    name = var.nic_name
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     
     ip_configuration {
-      name = "internal"
+      name = var.nic_ipconfig_name
       subnet_id = azurerm_subnet.subnet.id
-      private_ip_address_allocation = "Dynamic"
+      private_ip_address_allocation = var.nic_ip_allocation_method
     }
-    
     tags = {
       "CloudSkills" = "Challenge"
-    
     }
 }
 
 #Virtual Machine
 resource "azurerm_windows_virtual_machine" "vm" {
-    name = "CSBVM"
+    name = "terratest"
     resource_group_name = azurerm_resource_group.rg.name
-    size = "Standard_B1s"
+    size = var.vm_size
     location = azurerm_resource_group.rg.location
     admin_username = var.username  
     admin_password = var.securepassword
     network_interface_ids = [ azurerm_network_interface.nic.id ]
    
     os_disk {
-      caching = "ReadWrite"
-      storage_account_type = "Standard_LRS"
+      caching = var.os_disk_cache_type
+      storage_account_type = var.os_storage_type
     }
 
     source_image_reference {
-      publisher = "MicrosoftWindowsServer"
-      offer = "WindowsServer"
-      sku = "2016-Datacenter"
-      version = "latest"
+      publisher = var.os_publisher
+      offer = var.os_offer
+      sku = var.os_sku
+      version = var.os_version
     }
   
 }
--- Azure/Terraform/Virtual Machine Windows/variables.tf
+++ Azure/Terraform/Virtual Machine Windows/variables.tf
@@ -1,7 +1,7 @@
 variable "username" {
-    description = "Secure Username"
+    description = "Username"
     type = string
-    default = "Admin"
+    default = "benlocalaccount"
 }
 
 variable "securepassword" {
@@ -10,3 +10,104 @@ variable "securepassword" {
   sensitive = true
 }
 
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type = string
+  default = "Ben-Test-RG"
+}
+
+variable "location" {
+  description = "Region Location"
+  type = string
+  default = "australiasoutheast"
+}
+
+variable "vnet_name" {
+  description = "VNET Name"
+  type = string
+  default = "Ben_Test_Network"
+}
+
+variable "address_space" {
+  description = "Vnet Address Space"
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "Name of Subnet"
+  type = string
+  default = "Ben_Test_Subnet"
+}
+
+variable "subnet_address_range" {
+  description = "Subnet Address Prefix"
+  type =  list(string)
+  default = ["10.0.1.0/24"]
+}
+
+variable "nic_name" {
+  description = "Network Interface Name"
+  type = string
+  default = "ben_test_nic"
+}
+
+variable "nic_ipconfig_name" {
+  description = "NIC IP Configuration Name"
+  type = string
+  default = "ben_test_nic_ipconfig"
+}
+
+variable "nic_ip_allocation_method" {
+  description = "Static or Dynamic ip allocation method"
+  type = string
+  default = "Dynamic"
+}
+
+variable "vm_name" {
+  description = "Name of the Virtual Machine"
+  type = string
+  default = "ben_test_vm"
+}
+
+variable "vm_size" {
+  description = "VM Size"
+  type = string
+  default = "Standard_B1s"
+}
+
+variable "os_disk_cache_type" {
+  description = "OS Disk Cache Type"
+  type = string
+  default = "ReadWrite"
+}
+
+variable "os_storage_type" {
+  description = "OS Storage Type"
+  type = string
+  default = "Standard_LRS"
+}
+
+variable "os_publisher" {
+  description = "Publisher"
+  type = string
+  default = "MicrosoftWindowsServer"  
+}
+
+variable "os_offer" {
+  description = "Type of OS"
+  type = string
+  default = "WindowsServer"
+}
+
+variable "os_sku" {
+  description = "OS Version"
+  type = string
+  default = "2016-Datacenter"
+}
+
+variable "os_version" {
+  description = "OS Version"
+  type = string
+  default = "latest"
+}
